ebc838ccdafd7c18b72be44ecaeff8150fbb7af3
ekato.mp4 <evanbison25@gmail.com>
2024-10-14 10:43:44 -0400

    sliders

--- src/views/Account/index.tsx
+++ src/views/Account/index.tsx
@@ -1,31 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip'
 import styles from "./account.module.scss";
+import { useState } from 'react'
 
 export default function Account() {
 	const navigate = useNavigate();
 	const goToHome = () => {
         navigate('/');
     };
+	const [brightness, setBrightness] = useState(100)
 	return (
 		<>
-			<h1>Settings</h1>
-			<Tooltip className="tooltip" id="menu">Navigate to Menu</Tooltip>
-			<div className="homeButton" onClick={goToHome} data-tooltip-id='menu'>
-				<div /> <div /> <div />	
-			</div>
-			<div className={styles.settingsDiv}>
-				{["Effects", "Sound", "Music", "Light", "Hints", "Weight", "Auto Save", "Check In", "Notifs"].map((setting, index) => (
-					<div key={index}>
-						<p>{setting}</p>
-						<label className="switch">
-							<input type="checkbox" defaultChecked={setting !== "Light" && setting !== "Weight" && setting !== "Check In"} />
-							<span className="slider"></span>
-						</label>
-					</div>
-				))}
-			</div>
-			<div className={styles.rightBottomBox}>
+			<div className={styles.container}>
+				<h1>Settings</h1>
+				<Tooltip className="tooltip" id="menu">Navigate to Menu</Tooltip>
+				<div className="homeButton" onClick={goToHome} data-tooltip-id='menu'>
+					<div /> <div /> <div />	
+				</div>
+				<div className={styles.settings}>
+					{["Effects", "Sound", "Music", "Light", "Hints", "Weight", "Auto Save", "Check In", "Notifs"].map((setting, index) => (
+						<div key={index}>
+							<p>{setting}</p>
+							<label className="switch">
+								<input type="checkbox" defaultChecked={setting !== "Light" && setting !== "Weight" && setting !== "Check In"} />
+								<span className="toggle"></span>
+							</label>
+						</div>
+					))}
+				</div>
+				<div className={styles.settings}>
+					{[{name:"Brightness", min:0, max:100}].map((setting, index) => (
+						<div key={index}>
+							<p>{setting.name}</p>
+							<input type="range" min={setting.min} max={setting.max}  className="slider" value={brightness} onChange={e => setBrightness(e.target.value as any)} />
+						</div>
+					))}
+				</div>
 				<p>Creators: name, name, name</p>
 			</div>
 		</>
